c3ccbc1a41b0f0e6afdeb3437356ab8fc8c80a5b
洪坤 <@LAPTOP-004CSG1H>
2019-06-21 14:44:43 +0800

    hhh

---
+++ src/main/java/test/strutstest.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
+
+public class strutstest {
+	public String test(){
+		System.out.println("aaa");
+		return "success";
+	}
+}
